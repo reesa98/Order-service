6694a1587e3861a442e605d61d84f5ff665ac6aa
reesa98 <reesasusan@gmail.com>
2025-01-05 16:29:51 -0500

    Added 2 orders

--- src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
+++ src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
@@ -16,6 +16,8 @@ public class OrderDao {
                 new Order(58, "Book", 4, 2000),
                 new Order(205, "Laptop", 1, 150000),
                 new Order(809, "headset", 1, 1799),
+                new Order(111, "Pen", 5, 23232),
+                new Order(222, "SmartWatch", 7, 12121),
                 new Order(111, "Car", 1, 200000))
                 .collect(Collectors.toList());
     }
